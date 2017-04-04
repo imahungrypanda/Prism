2490798da79db857d826403efb693bf53f4ffcd8
Steve Olsen <stevelolsen@gmail.com>
2017-04-04 09:28:48 -0700

    Buttons work

--- popup.js
+++ popup.js
@@ -2,16 +2,27 @@ const displayFilter = filterName => {
   let el = document.getElementsByClassName(filterName)[0];
   console.log(el);
   el.style.display = "flex";
-}
+};
+
+const hideFilter = filterName => {
+  let el = document.getElementsByClassName(filterName)[0];
+  el.style.display = "none";
+};
+
+const clearFilter = list => () => {
+  list.forEach(li => hideFilter(li.textContent));
+};
 
 
 document.addEventListener('DOMContentLoaded', () => {
   let list = document.getElementsByTagName('li');
   list = Array.prototype.slice.call(list);
+  const clear = clearFilter(list);
 
   list.forEach(li => {
     li.addEventListener('click', e => {
       console.log(e.target.textContent);
+      clear();
 
       switch (e.target.textContent) {
         case "Protanopia":
@@ -20,8 +31,27 @@ document.addEventListener('DOMContentLoaded', () => {
         case "Protanomaly":
           displayFilter("Protanomaly");
           break;
-        default:
+        case "Deuteranopia":
+          displayFilter("Deuteranopia");
+          break;
+        case "Deuteranomaly":
+          displayFilter("Deuteranomaly");
+          break;
+        case "Tritanopia":
+          displayFilter("Tritanopia");
+          break;
+        case "Tritanomaly":
+          displayFilter("Tritanomaly");
+          break;
+        case "Achromatopsia":
+          displayFilter("Achromatopsia");
+          break;
+        case "Achromatomaly":
+          displayFilter("Achromatomaly");
+          break;
 
+        default:
+          break;
       }
 
     })
